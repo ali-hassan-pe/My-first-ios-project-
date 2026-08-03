937030b818c0bba7ad22dd20776ef02dae1adfe6
ali-hassan-pe <ali@sideline.agency>
2026-08-03 10:14:21 +0500

    Initial Commit

---
+++ Food delevery app/Food_delevery_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Food_delevery_appApp.swift
+//  Food delevery app
+//
+//  Created by Ali Hassan on 03/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct Food_delevery_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
